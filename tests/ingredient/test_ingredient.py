b59614f20a620108b843825205d540a95add0307
Rafael Soares <r.soares2015@outlook.com>
2023-06-17 17:06:51 -0300

    test: Implementa testes para a classe Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,7 +1,22 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
-# starting project
+
+BACON_HASH = hash('bacon')
+FARINHA_HASH = hash('farinha')
 
 
 # Req 1
 def test_ingredient():
-    pass
+    bacon = Ingredient('bacon')
+    farinha_de_trigo = Ingredient('farinha')
+    farinha_de_rosca = Ingredient('farinha')
+    presunto = Ingredient('presunto')
+
+    assert bacon != farinha_de_trigo
+    assert farinha_de_trigo == farinha_de_rosca
+    assert bacon.restrictions == presunto.restrictions
+    assert farinha_de_trigo.name == 'farinha'
+    assert hash(bacon) != hash(presunto)
+    assert hash(farinha_de_trigo) == hash(farinha_de_rosca)
+    assert repr(presunto) == "Ingredient('presunto')"
+    assert len(bacon.restrictions) == 2
+    assert len(farinha_de_rosca.restrictions) == 1
